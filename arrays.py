cc373143869b369ddf0f5ea001e9b7c2cc1d223c
Tomás Carvalho <tomasafonsoferreiracarvalho@gmail.com>
2025-11-17 21:48:48 +0000

    Add arrays.py initial script

---
+++ arrays.py
@@ -0,0 +1,15 @@
+list = ['banana','morango',4,6,8,'flauta']
+list = list
+print(list[0],list[4])
+print('A lista têm ',len(list),'elementos')
+print(list[1 : 4]) #os que estão dentro dos limites a conar com o ultimo
+print(list[1 : 4 : ]) #igual ao de cima
+print(list[1 : : 4]) #fora do intervalo
+print(list[-1 : : -1]) #começar do fundo
+print(list[: 0 : -1]) #sem o primeiro
+print(list[: -3 : 1])  #o 1 é o fim o -3 tira os 3 a esquerda
+print(list[ : : 2]) #anda de 2 em dois a partir do 1
+lista = [3,'cheila',10.8,68,'moto']
+lista = lista
+print(lista)
+print(list + lista) #soma das duas listas 
