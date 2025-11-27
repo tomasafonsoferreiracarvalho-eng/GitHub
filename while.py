b57fcaf2bd986e00694ff4c82767703d46c6ded5
Tomás Carvalho <tomasafonsoferreiracarvalho@gmail.com>
2025-11-27 11:51:41 +0000

    while.py

---
+++ while.py
@@ -0,0 +1,38 @@
+#i = 1
+#while i<10: # enquanto i for menor que 10 
+#   i += 1
+#   print(i)
+#print('Terminou')
+#print(i)
+
+#criancas = ['manu', 'celina', 'dudu']
+#for item in criancas:
+#   print(item)
+
+#canal = 'Refatorando'
+
+#for letra in canal:
+#    print(letra , end = ' ')
+#print()
+#for letra in canal:
+#    print(canal)
+#criancas = ['manu','violeta','dudu']
+#for index in range(len(criancas)):
+#   print(criancas[index],'\t', index)
+
+#for index in range(5):
+#    if index == 0:
+#        print('primeira linha')
+#    else:
+#        print(f'outras linhas {index}')
+
+#matrix_numbers = [[1,2,3],
+#                  [4,5,6],
+#                  [7,8,9],
+#                 [0],
+#                  ]
+#for line in matrix_numbers:
+#    #print(line)
+#    print('------')
+#    for coluna in line:
+#        print(coluna)
