255ac54cbc6d8db4041c6956c71c582d23cf0b92
Tomás Carvalho <tomasafonsoferreiracarvalho@gmail.com>
2025-11-27 20:28:57 +0000

    brincadeira.py

---
+++ brincadeira.py
@@ -0,0 +1,9 @@
+butterflies = 10
+beetles = 12
+ladybugs = 20
+
+print('I caught ' , butterflies ,' 🦋 butterflies!')
+print('I caught ' , beetles , ' 🪲 beetles!')
+print('I caught ' , ladybugs , ' 🐞 ladybugs!')
+total = butterflies+beetles+ladybugs
+print('I caught ' + str(total) + ' total bugs!')
